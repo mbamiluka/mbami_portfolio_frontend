d9824af364b7e823ed7ee81cc2c6c9cd8dcb17da
Mbami Luka <mbamiluka@gmail.com>
2024-07-05 02:37:25 -0400

    implement hide nav on scroll

--- src/components/molecules/NavBar.js
+++ src/components/molecules/NavBar.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import MenuButton from "../atoms/MenuButton";
 import { Link } from "react-router-dom";
 
 const NavBar = ({isActive}) => {
   const [isActivated, setIsActivated] = React.useState(false);
+  const [isVisible, setIsVisible] = React.useState(true);
+
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const threshold = 2; // Amount of scroll up required to show the nav
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      if (lastScrollTop > currentScrollPos && lastScrollTop - currentScrollPos > threshold) {
+        setIsVisible(true); // Show nav if scrolled up more than threshold
+      } else if (currentScrollPos > lastScrollTop && currentScrollPos - lastScrollTop > threshold) {
+        setIsVisible(false); // Hide nav if scrolled down more than threshold
+      }
+      setLastScrollTop(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollTop]);
+
   return (
-    <nav className={isActivated ? "" : "inactive"}>
+    <nav className={`${isActivated ? "" : "inactive"} ${isVisible ? "" : "hide"}`}>
         <MenuButton 
             isActive={isActivated}
             setIsActive={setIsActivated}
