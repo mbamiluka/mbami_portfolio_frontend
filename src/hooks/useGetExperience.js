ce480bff13fd101587a1da5e939a3f69c5600334
Mbami Luka <mbamiluka@gmail.com>
2024-07-23 04:33:46 -0400

    custom hook for getting different types of exp(work, edu, volun, etc) with req param

--- src/hooks/useGetExperience.js
+++ src/hooks/useGetExperience.js
@@ -1,1 +1,33 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+const apiUrl = process.env.REACT_APP_MBAMI_PORTFOLIO_BACKEND_URL_DEVELOPMENT;
+
+const useGetExperiences = (type) => {
+	const [experiences, setExperiences] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const fetchExperiences = async () => {
+			try {
+				const response = await fetch(`${apiUrl}/api/v1/experience?type=${type}`);
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+				const data = await response.json();
+				setExperiences(data);
+			} catch (error) {
+				setError(error);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchExperiences();
+	}, [type]);
+
+	return { experiences, loading, error };
+};
+
+export default useGetExperiences;
+
